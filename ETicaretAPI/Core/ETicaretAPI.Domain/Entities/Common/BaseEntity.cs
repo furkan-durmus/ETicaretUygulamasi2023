209752048510534ccd8141f7c9dfa713a2e9cf20
Furkan Durmus <16furkandurmus@gmail.com>
2023-04-08 16:20:09 +0300

    08

--- ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
+++ ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -9,6 +9,6 @@ namespace ETicaretAPI.Domain.Entities.Common
     public class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreateDate { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
--- ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Order.cs
+++ ETicaretAPI/Core/ETicaretAPI.Domain/Entities/Order.cs
@@ -9,5 +9,8 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class Order : BaseEntity
     {
+        public string Description { get; set; }
+        public string Adress { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
